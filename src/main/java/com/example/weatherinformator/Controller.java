083f116185b85429aefc0b8b61c551e705b3ba87
Ixor <dsv>
2022-10-27 14:09:22 +0300

    Meteoprog

--- src/main/java/com/example/weatherinformator/Controller.java
+++ src/main/java/com/example/weatherinformator/Controller.java
@@ -21,7 +21,7 @@ import javafx.scene.layout.AnchorPane;
 
 public class Controller {
     ArrayList<String> WeatherDays= new ArrayList<>();
-    ObservableList<String> sites = FXCollections.observableArrayList("Yandex","Gismeteo");
+    ObservableList<String> sites = FXCollections.observableArrayList("Yandex","Meteoprog");
 
     @FXML
     private ResourceBundle resources;
@@ -43,106 +43,164 @@ public class Controller {
         findweather.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                if (siteChange.getSelectionModel().getSelectedItem().equals("Yandex")) {
+                if (siteChange.getSelectionModel().getSelectedItem().equals("Meteoprog")) {
                     try {
-                        location = new URL("https://yandex.ru/pogoda/month");
+                        location = new URL("https://www.meteoprog.com/ru/weather/Penza/month/");
+                        StringBuilder siteHTML = new StringBuilder();
+                        try {
+                            URLConnection uc = location.openConnection();
+                            uc.connect();
+                            uc = location.openConnection();
+                            uc.addRequestProperty("User-Agent",
+                                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+                            try (BufferedReader br = new BufferedReader(new InputStreamReader(uc.getInputStream()))) {
+                                String i;
+                                while ((i = br.readLine()) != null) {
+                                    siteHTML.append(i);
+                                }
+                            } catch (Exception ex) {
+                                System.out.println(ex.getMessage());
+                            }
+                        } catch (Exception ex) {
+                            System.out.println(ex.getMessage());
+                        }
+                        String[] weatherData = siteHTML.toString().split("<div class=\"city-month__days\">")[1].split("°</span>");
+                        Pattern pattern = Pattern.compile("<div class=\"city-month__day(.*)");
+
+                        for (int i = 0; i < weatherData.length; i++) {
+                            Matcher matcher = pattern.matcher(weatherData[i]);
+                            if (matcher.find()) {
+                                WeatherDays.add(matcher.group());
+                            }
+                        }
+
+
+
+                        Pattern temperatureRegExp = Pattern.compile("[-+](\\d)|\\d°");
+                        Pattern dateRegExp = Pattern.compile("(\\d\\d|\\d) [А-я]+");
+                        Pattern weatherRegExp = Pattern.compile("title=\"([А-я]|,| )+");
+
+                        ArrayList<Day> dayArray = new ArrayList<>();
+
+                        for (int i = 0; i < WeatherDays.size(); i++) {
+                            int temperature = 0;
+                            String date = new String();
+                            String pressure = new String();
+                            String wet = new String();
+                            String windSpeed = new String();
+                            String weather = new String();
+                            Matcher matcher = temperatureRegExp.matcher(WeatherDays.get(i));
+                            if (matcher.find()) {
+                                temperature = Integer.parseInt(matcher.group().replaceAll("\\+|°",""));
+                            }
+                            matcher = dateRegExp.matcher(WeatherDays.get(i));
+                            if (matcher.find()) {
+                                date = matcher.group();
+                            }
+                            matcher = weatherRegExp.matcher(WeatherDays.get(i));
+                            if (matcher.find()) {
+                                weather = matcher.group().replace("title=\"","");
+                            }
+                            dayArray.add(new Day(temperature, wet, pressure, windSpeed, weather, date));
+                        }
+
                     } catch (MalformedURLException ex) {
                         System.out.println(ex.getMessage());
                     }
-                } else if (siteChange.getSelectionModel().getSelectedItem().equals("Gismeteo")) {
-                    //доделать
-                }
-
-                StringBuilder siteHTML = new StringBuilder();
-                try {
-                    URLConnection uc = location.openConnection();
-                    uc.connect();
-                    uc = location.openConnection();
-                    uc.addRequestProperty("User-Agent",
-                            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-                    try(BufferedReader br = new BufferedReader(new InputStreamReader(uc.getInputStream()))){
-                        String i;
-                        while((i = br.readLine())!=null ){
-                            siteHTML.append(i);
+                } else if (siteChange.getSelectionModel().getSelectedItem().equals("Yandex")) {
+                    try {
+                        location = new URL("https://yandex.ru/pogoda/month");
+                        StringBuilder siteHTML = new StringBuilder();
+                        try {
+                            URLConnection uc = location.openConnection();
+                            uc.connect();
+                            uc = location.openConnection();
+                            uc.addRequestProperty("User-Agent",
+                                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+                            try (BufferedReader br = new BufferedReader(new InputStreamReader(uc.getInputStream()))) {
+                                String i;
+                                while ((i = br.readLine()) != null) {
+                                    siteHTML.append(i);
+                                }
+                            } catch (Exception ex) {
+                                System.out.println(ex.getMessage());
+                            }
+                        } catch (Exception ex) {
+                            System.out.println(ex.getMessage());
                         }
-                    }
-                    catch (Exception ex){
+                        String[] weatherData = siteHTML.toString().split("</span></div></div></div></div></div></div></div>");
+
+                        Pattern pattern = Pattern.compile("<div class=\"climate-calendar-day( climate-calendar-day_holiday | climate-calendar-day_colorless_yes |\\s)climate-calendar-day_with-history\"(.*)");
+
+                        for (int i = 0; i < weatherData.length; i++) {
+                            Matcher matcher = pattern.matcher(weatherData[i]);
+                            if (matcher.find()) {
+                                WeatherDays.add(matcher.group());
+                            }
+                        }
+
+                        Pattern temperatureRegExp = Pattern.compile("temp__value temp__value_with-unit\">([+−])(\\d|\\d\\d)<");
+                        Pattern dateRegExp = Pattern.compile("(\\d\\d|\\d) [А-я]+");
+                        Pattern pressureRegExp = Pattern.compile("\\d\\d\\d мм рт\\. ст\\.");
+                        Pattern wetRegExp = Pattern.compile("\\d\\d%");
+                        Pattern windSpeedRegExp = Pattern.compile("\"wind-speed\">((\\d\\.\\d)|(\\d))");
+                        Pattern weatherRegExp = Pattern.compile("src=\"//yastatic\\.net/weather/i/icons/funky/dark/(\\w|-)+\\.svg\"");
+
+                        ArrayList<Day> dayArray = new ArrayList<>();
+
+                        for (int i = 0; i < WeatherDays.size(); i++) {
+                            int temperature = 0;
+                            String date = new String();
+                            String pressure = new String();
+                            String wet = new String();
+                            String windSpeed = new String();
+                            String weather = new String();
+                            Matcher matcher = temperatureRegExp.matcher(WeatherDays.get(i));
+                            if (matcher.find()) {
+                                temperature = Integer.parseInt(matcher.group().replaceAll("temp__value temp__value_with-unit\">|<", ""));
+                            }
+                            matcher = dateRegExp.matcher(WeatherDays.get(i));
+                            if (matcher.find()) {
+                                date = matcher.group().replaceAll("\"climate-calendar-day__detailed-day\">", "");
+                            }
+                            matcher = pressureRegExp.matcher(WeatherDays.get(i));
+                            if (matcher.find()) {
+                                pressure = matcher.group();
+                            }
+                            matcher = wetRegExp.matcher(WeatherDays.get(i));
+                            if (matcher.find()) {
+                                wet = matcher.group();
+                            }
+                            matcher = windSpeedRegExp.matcher(WeatherDays.get(i));
+                            if (matcher.find()) {
+                                windSpeed = matcher.group().replace("\"wind-speed\">", "") + " м/c";
+                            }
+                            matcher = weatherRegExp.matcher(WeatherDays.get(i));
+                            if (matcher.find()) {
+                                weather = matcher.group().replaceAll("src=\"//yastatic\\.net/weather/i/icons/funky/dark/|\"|\\.svg", "");
+                                switch (weather) {
+                                    case "ovc_-ra":
+                                    case "ovc_ra":
+                                        weather = "Дождь";
+                                        break;
+                                    case "bkn_d":
+                                        weather = "Облачно с прояснениями";
+                                        break;
+                                    case "ovc":
+                                        weather = "Облачно";
+                                        break;
+                                    case "ovc_-sn":
+                                        weather = "Снег";
+                                        break;
+                                }
+                            }
+                            dayArray.add(new Day(temperature, wet, pressure, windSpeed, weather, date));
+                        }
+
+                    } catch (MalformedURLException ex) {
                         System.out.println(ex.getMessage());
                     }
                 }
-                catch (Exception ex){
-                    System.out.println(ex.getMessage());
-                }
-                String[] weatherData = siteHTML.toString().split("</span></div></div></div></div></div></div></div>");
-
-                Pattern pattern = Pattern.compile("<div class=\"climate-calendar-day( climate-calendar-day_holiday | climate-calendar-day_colorless_yes |\\s)climate-calendar-day_with-history\"(.*)");
-
-                for (int i = 0; i< weatherData.length;i++){
-                    Matcher matcher = pattern.matcher(weatherData[i]);
-                    if(matcher.find()){
-                        WeatherDays.add(matcher.group());
-                    }
-                }
-
-                Pattern temperatureRegExp = Pattern.compile("temp__value temp__value_with-unit\">([+−])(\\d|\\d\\d)<");
-                Pattern dateRegExp = Pattern.compile("(\\d\\d|\\d) [А-я]+");
-                Pattern pressureRegExp = Pattern.compile("\\d\\d\\d мм рт\\. ст\\.");
-                Pattern wetRegExp = Pattern.compile("\\d\\d%");
-                Pattern windSpeedRegExp = Pattern.compile("\"wind-speed\">((\\d\\.\\d)|(\\d))");
-                Pattern weatherRegExp = Pattern.compile("src=\"//yastatic\\.net/weather/i/icons/funky/dark/(\\w|-)+\\.svg\"");
-
-                ArrayList<Day> dayArray = new ArrayList<>();
-
-                for (int i = 0; i< WeatherDays.size();i++){
-                    int temperature = 0;
-                    String date = new String();
-                    String pressure = new String();
-                    String wet = new String();
-                    String windSpeed = new String();
-                    String weather = new String();
-                    Matcher matcher = temperatureRegExp.matcher(WeatherDays.get(i));
-                    if(matcher.find()){
-                        temperature = Integer.parseInt(matcher.group().replaceAll("temp__value temp__value_with-unit\">|<",""));
-                    }
-                    matcher = dateRegExp.matcher(WeatherDays.get(i));
-                    if(matcher.find()){
-                        date = matcher.group().replaceAll("\"climate-calendar-day__detailed-day\">","");
-                    }
-                    matcher = pressureRegExp.matcher(WeatherDays.get(i));
-                    if(matcher.find()){
-                        pressure = matcher.group();
-                    }
-                    matcher = wetRegExp.matcher(WeatherDays.get(i));
-                    if(matcher.find()){
-                        wet = matcher.group();
-                    }
-                    matcher = windSpeedRegExp.matcher(WeatherDays.get(i));
-                    if(matcher.find()){
-                        windSpeed = matcher.group().replace("\"wind-speed\">","")+" м/c";
-                    }
-                    matcher = weatherRegExp.matcher(WeatherDays.get(i));
-                    if(matcher.find()){
-                        weather = matcher.group().replaceAll("src=\"//yastatic\\.net/weather/i/icons/funky/dark/|\"|\\.svg","");
-                        switch (weather){
-                            case "ovc_-ra":
-                            case "ovc_ra":
-                                weather = "Дождь";
-                                break;
-                            case "bkn_d":
-                                weather = "Облачно с прояснениями";
-                                break;
-                            case "ovc":
-                                weather = "Облачно";
-                                break;
-                            case "ovc_-sn":
-                                weather = "Снег";
-                                break;
-                        }
-                    }
-                    dayArray.add(new Day(temperature,wet,pressure,windSpeed,weather,date));
-                }
-
-
             }
         });
     }
